df9bcfb05e7c89fc1fce675887e1955059f91f6c
ashraf <sadarsign@gmail.com>
2025-02-09 18:14:18 +0300

    Initialized

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,24 @@
 import express from "express"
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
+import  dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-app.use(helmet()) //helmet is a secrity middleware that helps you to protect your app by setting various HTTP headers
+const PORT = process.env.PORT
 
-app.get("/", (req, res) => {
-    console.log(res.getHeader());
-    res.send("Hello from the backend")
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet()) //helmet is a secrity middleware that helps you to protect your app by setting various HTTP headers
+app.use(morgan("dev")); //log the requests
+app.get("/test", (req, res) => {
+    res.send("Hello from the test route")
 })
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
+app.listen(PORT, () => {
+    // console.log(res.getHeader());
+    console.log(`Server running on port ${PORT}`)
 });
