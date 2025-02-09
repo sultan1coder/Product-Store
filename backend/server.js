e0d9f0b44b06a71d00cec14e3644ca6c6a68d9f0
ashraf <sadarsign@gmail.com>
2025-02-09 21:42:02 +0300

    Initialized

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ import express from "express"
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
-import  dotenv from "dotenv";
+import dotenv from "dotenv";
 
 
 import productRoute from "./routes/productRoute.js";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -17,10 +18,29 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(cors());
 app.use(helmet()) //helmet is a secrity middleware that helps you to protect your app by setting various HTTP headers
-app.use(morgan("dev")); //log the requests
-app.use("/api/products", productRoute)
 
-app.listen(PORT, () => {
-    // console.log(res.getHeader());
-    console.log(`Server running on port ${PORT}`)
+app.use(morgan("dev")); //log the requests
+
+app.use("/api/products", productRoute);
+
+async function initDB() {
+    try {
+        await sql`
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10, 2) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        `;
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.log("Error initDB", error);
+    }
+}
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    });
 });
