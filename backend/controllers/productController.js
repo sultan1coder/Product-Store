d075a01f9af992490d544803336c58c51419f6d0
ashraf <sadarsign@gmail.com>
2025-02-10 19:12:57 +0300

    Initialized

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -65,5 +65,34 @@ export const getProduct = async (req, res) => {
         });
     }
 }
-export const updateProduct = async (req, res) => { }
+export const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const { name, price, image } = req.body;
+    try {
+        const updateProduct = await sql`
+        UPDATE products
+        SET name=${name}, price=${price}, image=${image}
+        WHERE id=${id}
+        RETURNING *
+        `;
+
+        if (updateProduct.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "Product not found"
+            })
+
+            res.status(200).json({
+                success: true,
+                data: updateProduct[0]
+            });
+        }
+    } catch (error) {
+        console.log("Error in updateProduct function", error);
+        res.status(500).json({
+            seccess: false,
+            message: "Internal Server Error"
+        });
+    }
+}
 export const deleteProduct = async (req, res) => { }
