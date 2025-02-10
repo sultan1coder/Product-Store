905f5cc7eecd343479d419193f9e21553f5c26ce
ashraf <sadarsign@gmail.com>
2025-02-10 19:21:56 +0300

    Initialized

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -95,4 +95,30 @@ export const updateProduct = async (req, res) => {
         });
     }
 }
-export const deleteProduct = async (req, res) => { }
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedProduct = await sql`
+        DELETE FROM products WHERE id=${id} RETURNING *
+        `;
+
+        if (deletedProduct.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: deletedProduct[0]
+        });
+    } catch (error) {
+        console.log("Error in deleteProduct function", error);
+        res.status(500).json({
+            seccess: false,
+            message: "Internal Server Error"
+        });
+    }
+}
