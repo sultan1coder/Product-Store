036a1fd34344787bc7dbaf7e5ac2fb3853a2dcb8
ashraf <sadarsign@gmail.com>
2025-02-10 17:58:14 +0300

    Initialized

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,2 +1,52 @@
-export const getAllProducts = async (req, res) => { }
-export const createProducts = async (req, res) => { }
+import { sql } from "../config/db"
+export const getProducts = async (req, res) => {
+    try {
+        const products = await sql`
+        SELECT * FROM products 
+        ORDER BY created_at DESC
+        `;
+        console.log("fetched products", products);
+        res.status(200).json({
+            success: true,
+            data: products
+        })
+    } catch (error) {
+        console.log("Error in getProducts function", error);
+        res.status(500).json({
+            seccess: false,
+            message: "Internal Server Error"
+        });
+    }
+}
+export const createProducts = async (req, res) => {
+    const { name, price, image } = req.body;
+    if (!name || !price || !image) {
+        return res.status(400).json({
+            success: false,
+            message: "All fields are required"
+        })
+    }
+    try {
+        const newProduct = await sql`
+        INSERT INTO products (name,price,image)
+        VALUES (${name},${price}),${image}
+        RETURNING *
+        `;
+        console.log("New Product added", newProduct)
+        res.status(201).json({
+            success: true,
+            data: newProduct
+        })
+
+    } catch (error) {
+        console.log("Error in createProducts function", error);
+        res.status(500).json({
+            seccess: false,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export const getProduct = async (req, res) => { }
+export const updateProduct = async (req, res) => { }
+export const deleteProduct = async (req, res) => { }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createProducts, getAllProducts } from "../controllers/productController.js";
+import { createProducts, deleteProduct, getProduct, getProducts, updateProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
+router.get("/", getProducts);
+router.get("/:id", getProduct);
 router.post("/", createProducts);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 
 export default router;
