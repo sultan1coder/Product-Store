ce117a2d0e15d928fd7300539ee61962c64f3d9d
ashraf <sadarsign@gmail.com>
2025-04-04 16:27:03 +0300

    Initialized

--- backend/server.js
+++ backend/server.js
@@ -12,50 +12,53 @@ import { aj } from "./lib/arcjet.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet()) //helmet is a secrity middleware that helps you to protect your app by setting various HTTP headers
+app.use(helmet()); //helmet is a secrity middleware that helps you to protect your app by setting various HTTP headers
 
 app.use(morgan("dev")); //log the requests
 
 app.use(async (req, res, next) => {
-    try {
-        const decision = await aj.protect(req, {
-            requested: 1 //specifies that each request consumes 1 token
-        });
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, //specifies that each request consumes 1 token
+    });
 
-        if (decision.isDenied()) {
-            if (decision.reason.isRateLimit()) {
-                res.status(429).json({ Erorr: "Too mant request" });
-            } else if (decision.reason.isBot()) {
-                res.status(403).json({ Error: "Bot access denied" });
-            } else {
-                res.status(403).json({ error: "Forbidden" });
-            }
-            return
-        }
-
-        //check for spoofed bots
-        if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
-            res.status(403).json({ error: "Spoofed bot detected" });
-            return
-        }
-
-        next();
-    } catch (error) {
-        console.log("Arcjet error", error);
-        next(error);
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ Erorr: "Too mant request" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ Error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
     }
-})
+
+    //check for spoofed bots
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoute);
 
 async function initDB() {
-    try {
-        await sql`
+  try {
+    await sql`
         CREATE TABLE IF NOT EXISTS products (
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
@@ -64,13 +67,13 @@ async function initDB() {
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
         `;
-        console.log("Database initialized successfully");
-    } catch (error) {
-        console.log("Error initDB", error);
-    }
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
 }
 initDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
