5dfcbd4a5f2ab7d159e91a6c32d62a6e9563bbd0
ashraf <sadarsign@gmail.com>
2025-04-04 16:26:40 +0300

    Initialized

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -4,25 +4,25 @@ import "dotenv/config";
 
 //init arcjet
 export const aj = arcjet({
-    key: process.env.ARCJET_KEY,
-    characteristics: ["ip.src"],
-    rules: [
-        //shield protects your app from common attacks e.g. SQL injection , CSRF attacks
-        shield({ mode: "LIVE" }),
-        detectBot({
-            mode: "LIVE",
-            //block all bots except search engines
-            allow: [
-                "CATEGORY: SEARCH_ENGINE"
-                //see the full list at https://arcjet.com/bot-list
-            ],
-        }),
-        //rate limiting
-        tokenBucket({
-            mode: "LIVE",
-            refillRate: 5,
-            interval: 10,
-            capacity: 10,
-        })
-    ]
-})
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    //shield protects your app from common attacks e.g. SQL injection , CSRF attacks
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      //block all bots except search engines
+      allow: [
+        "CATEGORY: SEARCH_ENGINE",
+        //see the full list at https://arcjet.com/bot-list
+      ],
+    }),
+    //rate limiting
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
