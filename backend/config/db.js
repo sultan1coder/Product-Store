d34824cd0f50fa3b2f40ee7df60ba816fc7567ef
ashraf <sadarsign@gmail.com>
2025-04-04 16:26:11 +0300

    Initialized

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 //creates a SQL connection using our env variables
 export const sql = neon(
-    `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
+  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
 );
 
 //this sql function we export is used as a tagged template literal, which allows us to write SQL quaries safely.
