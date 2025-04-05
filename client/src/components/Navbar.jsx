742ac2bad8d171ad6feb1cedaf2d30dbcbfcd09c
ashraf <sadarsign@gmail.com>
2025-04-05 18:23:26 +0300

    Initialized

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,9 +18,7 @@ function Navbar() {
             <Link to="/" className="transition-opacity hover:opacity-80">
               <div className="flex items-center gap-2">
                 <ShoppingCartIcon className="size-9 text-primary" />
-                <span
-                  className="font-mono text-2xl font-semibold tracking-widest text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary"
-                >
+                <span className="font-mono text-2xl font-semibold tracking-widest text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
                   POSGRESTORE
                 </span>
               </div>
